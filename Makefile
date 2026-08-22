691052be484820dc0622924fb32af3b5fb8ca5af
MonkeyBsness <w664351423@gmail.com>
2026-08-22 07:57:05 -0600

    finish pipe

--- Makefile
+++ Makefile
@@ -147,6 +147,7 @@ UPROGS=\
 	$U/_dorphan\
 	$U/_sync\
 	$U/_sleep\
+	$U/_pingpong\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
--- user/pingpong.c
+++ user/pingpong.c
@@ -2,8 +2,46 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(argc, char *argv[]){
+int main(int argc, char *argv[]){
 
-    int pid = fork();
+    int p[2];
+    int p1[2];
+    char buf[10];
+
+    pipe(p);
+    pipe(p1);
+
+
+    if (fork() == 0){
+        close(p[1]);
+        close(p1[0]);
+        int n;
+        for(;;){
+            n = read(p[0], buf, 1);
+            if (n == 0){
+                break;
+            }
+        }
+        close(p[0]);
+        printf("%d :received ping\n", getpid());
+        write(p1[1], "a", 1);
+        close(p1[1]);
+
+
+    }else {
+        close(p[0]);
+        close(p1[1]);
+        int i;
+        write(p[1], "a", 1);
+        close(p[1]);
+        for(;;){
+            i = read(p1[0], buf, 1);
+            if (i == 0){
+                break;
+            }
+        }
+        printf("%d :received pong\n", getpid());
+        close(p1[0]);
+    }
     exit(0);
 }
